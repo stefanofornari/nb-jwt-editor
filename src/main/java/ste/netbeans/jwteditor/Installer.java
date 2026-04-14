c0152b09f0315be85b8defbf3b7b9918b7be84a1
Stefano Fornari <stefano.fornari@gmail.com>
2026-04-14 07:57:12 +0200

    Fixed #5 Module window opens with empty content after closing it

--- src/main/java/ste/netbeans/jwteditor/Installer.java
+++ src/main/java/ste/netbeans/jwteditor/Installer.java
@@ -1,11 +1,26 @@
 package ste.netbeans.jwteditor;
 
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import org.openide.modules.ModuleInstall;
 
 public class Installer extends ModuleInstall {
 
+    final private Logger LOG = Logger.getLogger(getClass().getName());
+
     @Override
     public void restored() {
-        // Module initialization code if needed
+        LOG.info("restoring JWTEditor plug-in");
+        Platform.setImplicitExit(false);
+    }
+
+    @Override
+    public boolean closing() {
+        LOG.info("closing JWTEditor plug-in");
+        try {
+            Platform.exit();
+        } catch (Exception x) {}
+
+        return true;
     }
 }
